a72c31a72c605c7dd936cf4ee279933ae5df8d74
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:35:35 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,35 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "LH", // Header Initials
-    heroTagline: "Lisa Harris Realty", // Hero Tagline
-    agentName: "Lisa Harris",
-    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.", // AboutMe Short Description
-    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio.
+    headerInitials: "LS",
+    heroTagline: "Lana Shoemaker Realty",
+    agentName: "Lana Shoemaker",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
-With my knowledge and experience, I provide my clients with accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
+With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
-Customer Service excellence is our Passion!`, // AboutMe Detailed Description
-    testimonials: [
-        { quote: "Working with Lisa was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-        { quote: "Lisa Harris Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-        { quote: "The dedication and expertise of Lisa were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-        { quote: "Lisa and her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-        { quote: "If you’re looking for a real estate agent who goes above and beyond, Lisa is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "LHarris@remax.net", // Email
-    address: "1225 A Tuscany Drive, Braselton, Georgia 30517", // Address
-    phone: "404-307-9898", // Phone Number
-    footerDescription: "Lisa Harris Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with Lana Shoemaker was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Lana Shoemaker provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Lana Shoemaker were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Lana Shoemaker and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Lana Shoemaker is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "",
+    address: "AL, USA",
+    phone: "251-504-4096",
+    footerDescription: `Lana Shoemaker Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
+        facebook: "https://www.facebook.com/lanasviatlana.shoemakerdziarneika/",
         Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
-        email: "LHarris@remax.net",
-        phone: "404-307-9898",
-    }, // Contact Details
+        email: "",
+        phone: "251-504-4096",
+    },
+    website: "",
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/246478574_5142305485797179_6275318756670774075_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=cE4n588QCwQQ7kNvgGbzPgq&_nc_ht=scontent-sjc3-1.xx&oh=00_AYBRG_wvCOOgn84SvxaS9FqJMVRyfYVcE5IsiIuM-oHTCQ&oe=66D7D6FB" // Agent image URL
 };
 
 export default clientData;
